66d340952a07b8f5e127353afa095cc1fbde130b
imnotAnsh01 <149237843+imnotAnsh01@users.noreply.github.com>
2026-01-21 21:59:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const menu = document.getElementById("menu");
-const menuBtn = document.getElementById("menuBtn");
-const closeBtn = document.getElementById("closeBtn");
-const overlay = document.getElementById("overlay");
+document.addEventListener("DOMContentLoaded", function(){
 
-menuBtn.addEventListener("click", () => {
-    menu.style.right = "0px";
-    overlay.style.opacity = "1";
-    overlay.style.visibility = "visible";
-    overlay.style.pointerEvents = "auto";
+    const menu = document.getElementById("menu");
+    const menuBtn = document.getElementById("menuBtn");
+    const closeBtn = document.getElementById("closeBtn");
+    const overlay = document.getElementById("overlay");
+
+    menuBtn.addEventListener("click", function(){
+        menu.style.right = "0px";
+        overlay.style.opacity = "1";
+        overlay.style.visibility = "visible";
+        overlay.style.pointerEvents = "auto";
+    });
+
+    closeBtn.addEventListener("click", closeMenu);
+    overlay.addEventListener("click", closeMenu);
+
+    function closeMenu(){
+        menu.style.right = "-280px";
+        overlay.style.opacity = "0";
+        overlay.style.visibility = "hidden";
+        overlay.style.pointerEvents = "none";
+    }
+
 });
-
-closeBtn.addEventListener("click", closeMenu);
-overlay.addEventListener("click", closeMenu);
-
-function closeMenu(){
-    menu.style.right = "-280px";
-    overlay.style.opacity = "0";
-    overlay.style.visibility = "hidden";
-}
